94d1b7d299dca5ec3d21d0cdbfe0d0d31358305c
mariia <111996419+MariiaS1710@users.noreply.github.com>
2023-03-14 17:57:20 +0100

    wright 01-color-switchers.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStop.disabled = true;
+let colorInterval = null;
+
+btnStart.addEventListener('click', () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    colorInterval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+})
+
+btnStop.addEventListener('click', () => {
+    clearInterval(colorInterval);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+
+})
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
