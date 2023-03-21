acfe5d4419551a159acd76718e4c2407a7821ecd
mariia <111996419+MariiaS1710@users.noreply.github.com>
2023-03-21 23:44:54 +0100

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from "notiflix";
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnSubmit = document.querySelector('button[type="submit"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        Reject({ position, delay });
+      }
+    }, delay);
+});
+  return promise;
+};
+
+btnSubmit.addEventListener('click', e => {
+  e.preventDefault();
+  let firstDelay = Number(delay.value); // конвертація затримки в ЧИСЛО
+  let delayStep = Number(step.value); // конвертація кроку в ЧИСЛО
+
+  for (let i = 0; i < amount.value; i += 1) {
+        createPromise(1 + i, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
-}
+  form.reset();
+});
+  
